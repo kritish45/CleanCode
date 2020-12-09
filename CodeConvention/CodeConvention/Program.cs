6b820327d595ab4eb727a4ee67bdbb4d8f2dd511
kritish45 <72802930+kritish45@users.noreply.github.com>
2020-12-09 15:00:46 +0400

    Add files via upload

---
+++ CodeConvention/CodeConvention/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeConvention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TestClass
+    {
+        private string _myString;
+
+        public TestClass(string myString)
+        {
+            this._myString = myString;
+        }
+
+        public string getString()
+        {
+            return _myString;
+        }
+
+        private void SetString(string myString)
+        {
+            this._myString = myString;
+        }
+    }
+}
